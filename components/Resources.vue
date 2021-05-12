44118cf8a86a08a24a65f2eab6a2ab5eb0ee39d1
Nim Anderson <danderson@allvuesystems.com>
2021-05-12 09:22:00 +0100

    Initial Commit

---
+++ components/Resources.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="grid">
+    <vs-row vs-w="12" type="flex" justify="center" align="center">
+      <vs-select v-model="value" placeholder="Select">
+        <vs-option label="Vuesax" value="1"> Vuesax </vs-option>
+        <vs-option label="Vue" value="2"> Vue </vs-option>
+        <vs-option label="Javascript" value="3"> Javascript </vs-option>
+        <vs-option disabled label="Sass" value="4"> Sass </vs-option>
+        <vs-option label="Typescript" value="5"> Typescript </vs-option>
+        <vs-option label="Webpack" value="6"> Webpack </vs-option>
+        <vs-option label="Nodejs" value="7"> Nodejs </vs-option>
+      </vs-select>
+      <vs-select v-if="hide" v-model="value" placeholder="Select">
+        <vs-option label="Vuesax" value="1"> Vuesax </vs-option>
+        <vs-option label="Vue" value="2"> Vue </vs-option>
+        <vs-option label="Javascript" value="3"> Javascript </vs-option>
+        <vs-option disabled label="Sass" value="4"> Sass </vs-option>
+        <vs-option label="Typescript" value="5"> Typescript </vs-option>
+        <vs-option label="Webpack" value="6"> Webpack </vs-option>
+        <vs-option label="Nodejs" value="7"> Nodejs </vs-option>
+      </vs-select>
+    </vs-row>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class Resources extends Vue {
+  value = ''
+  hide = false
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="container">
+    <div>
+      <header class="content-logos">
+        <logo />
+      </header>
+      <h1 class="title">Equipment Support Resource</h1>
+      <h2 class="subtitle">
+        Use the dropdowns below to pull up useful resources
+      </h2>
+      <Resources />
+      <div class="links">
+        <h3 class="h3">Can't find what you are looking for?</h3>
+        <p>
+          Please email
+          <a href="mailto:LTLC@hee.nhs.uk">LTLC@hee.nhs.uk</a> stating the type,
+          make and model of the equipment and we will do our best to add some
+          resources.
+        </p>
+      </div>
+      <div class="links">
+        <a
+          href="https://www.e-lfh.org.uk/programmes/london-transformation-and-learning-collaborative-ltlc/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Other LTLC resources
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import Resources from '~/components/Resources.vue'
+
+export default {
+  components: {
+    Logo,
+    Resources,
+  },
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 55px;
+  color: #35495e;
+  letter-spacing: 1px;
+  text-transform: capitalize;
+  margin: 25px 0;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 1.1rem;
+  color: #526488;
+  word-spacing: 2px;
+  padding-bottom: 15px;
+  margin: 25px 0;
+}
+
+.subtitle a {
+  font-weight: 500;
+  color: inherit;
+}
+
+.links {
+  padding-top: 15px;
+  margin-bottom: 20px;
+}
+
+.content-logos {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-width: 500px;
+}
+
+.plus {
+  font-size: 2.5rem;
+  margin: 15px;
+  color: #35495e;
+}
+
+.h3 {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-weight: 300;
+  margin: 10px;
+}
+
+.h3 p {
+  font-size: 1rem;
+}
+</style>
