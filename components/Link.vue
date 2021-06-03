960bf4f7215bb4db1546ea0c444972360c85aec4
Nim Anderson <d.and3rs0n@googlemail.com>
2021-06-03 22:06:11 +0100

    Refactor / Cleanup Code
    Extract and clean up components

---
+++ components/Link.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <a
+      v-for="link in links"
+      :key="link.url"
+      :href="link.url"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <b-button type="is-primary" outlined class="margin">
+        {{ link.linkType }}
+      </b-button>
+    </a>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator'
+import { ILink } from '~/interfaces'
+
+@Component
+export default class Link extends Vue {
+  @Prop({ required: true }) readonly links!: ILink[]
+}
+</script>
+
+<style scoped>
+.margin {
+  margin: 5%;
+}
+</style>
